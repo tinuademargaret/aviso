a722c60e860f0835f65334206435ecf1cd3beb92
tinumide <summittinuade@gmail.com>
2020-04-19 17:42:11 +0100

    email service

--- email.service.js
+++ email.service.js
@@ -1,34 +1,47 @@
 const redis = require('redis');
 const subscriber = redis.createClient();
+const config = require('./src/config')
+var nodemailer = require('nodemailer');
+subscriber.subscribe('service');
 
 console.log('ready to consume');
 subscriber.on('message', function (channel, message) {
  console.log('Message: ' + message + ' on channel: ' + channel + ' is arrive!');
+
+
+const notification = JSON.parse(message)
+const sender = notification.sender
+const recipient = notification.recipient
+const msg = notification.message
+
+console.log(config.email_address, config.email_password)
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  secureConnection: true,
+  port: 465,
+  auth: {
+    user: config.email_address,
+    pass: config.email_password
+  },
+  tls: {
+    secureProtocol: "TLSv1_method"
+}
 });
 
-subscriber.subscribe('service');
 
-// var nodemailer = require('nodemailer');
+var mailOptions = {
+  from: config.email_address,
+  to: recipient , 
+  subject: 'Notification from '+ sender + ' on slack',
+  text: msg
+};
 
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-    // user: 'youremail@gmail.com',
-    // pass: 'yourpassword'
-//   }
-// });
-
-// var mailOptions = {
-//   from: 'youremail@gmail.com',
-//   to: 'myfriend@yahoo.com',
-//   subject: 'Sending Email using Node.js',
-//   text: 'That was easy!'
-// };
-
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-    // console.log(error);
-//   } else {
-    // console.log('Email sent: ' + info.response);
-//   }
-// }); 
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
+});
--- src/config/index.js
+++ src/config/index.js
@@ -11,5 +11,7 @@ module.exports = {
     db_host: process.env.DB_HOST,
     db_user: process.env.DB_USER,
     db_password: process.env.DB_PASSWORD,
-    db_name: process.env.DB_NAME
+    db_name: process.env.DB_NAME,
+    email_address: process.env.EMAIL_ADDRESS,
+    email_password: process.env.EMAIL_PASSWORD
 }
