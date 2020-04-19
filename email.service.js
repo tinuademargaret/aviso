8e7ac4c6a27c4c650f4601aae334a76cc5f681d2
tinumide <summittinuade@gmail.com>
2020-04-19 11:32:50 +0100

    updates

--- email.service.js
+++ email.service.js
@@ -8,3 +8,27 @@ subscriber.on('message', function (channel, message) {
 
 subscriber.subscribe('service');
 
+// var nodemailer = require('nodemailer');
+
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+    // user: 'youremail@gmail.com',
+    // pass: 'yourpassword'
+//   }
+// });
+
+// var mailOptions = {
+//   from: 'youremail@gmail.com',
+//   to: 'myfriend@yahoo.com',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+    // console.log(error);
+//   } else {
+    // console.log('Email sent: ' + info.response);
+//   }
+// }); 
